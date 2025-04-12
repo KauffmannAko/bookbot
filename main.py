c81ae32c339a2959ad5ca564bb8f552e47faf00e
KauffmannAko <nanakauffmann@gmail.com>
2025-04-12 14:14:20 +0100

    implemented get_book_text function

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def main(filepath):
+    book_text = get_book_text(filepath)
+    word_count = get_word_count(book_text)
+    print(word_count)
+
+def get_book_text(filepath):
+    with open(filepath, "r") as f:
+        return f.read()
+
+
+
+
+main("books/frankenstein.txt")
