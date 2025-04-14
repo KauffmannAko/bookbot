f012e1f33b74aa964fcf03247229a55c8d3547fa
KauffmannAko <nanakauffmann@gmail.com>
2025-04-14 22:01:44 +0100

    implemented the report_stats function

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from stats import get_word_count, get_a_character_count
+from stats import get_word_count, get_a_character_count, report_stats
 
 def main(filepath, char_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
     book_text = get_book_text(filepath)
     word_count = get_word_count(book_text)
+    print("----------- Word Count ----------")
     char_count = get_a_character_count(book_text, char_list)
     print(word_count)
-    print(char_count)
+    report_stats(char_count)
 
 def get_book_text(filepath):
     with open(filepath, "r") as f:
         return f.read()
 
 
-main("books/frankenstein.txt",'tpc')
+main("books/frankenstein.txt",'et')
--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 def get_word_count(text):
     list_of_word = text.split()
-    return f"{len(list_of_word)} words found in the document"
+    return f"Found {len(list_of_word)} total words"
 
 def get_a_character_count(text, chars):
     char_count = {}
@@ -9,3 +9,15 @@ def get_a_character_count(text, chars):
         char_count[char.lower()] = lower_text.count(char.lower())
  
     return char_count
+
+def report_stats(dic):
+    print("--------- Character Count -------")
+    lstBook = list(dic.items())
+    lstBook.sort(key=lambda x: x[1], reverse=True)
+    sorted_dic = dict(lstBook)
+    for key, value in sorted_dic.items():
+        if not key.isalpha():
+            continue
+        else:
+            print(f"{key}: {value}")
+    print("============= END ===============")
